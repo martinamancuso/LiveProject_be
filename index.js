c288224c919142fa6c3b1dcd7379afa23de9ec28
Davide Lodde <johndoe@example.com>
2024-04-23 18:38:23 +0200

    23 aprile

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-import { fakeDB } from "./fake_database";
+// import { fakeDB } from "./fake_database";
 
 const express = require("express");
 
 const app = express();
 const port = 3000;
+
+
 
 app.get("/backendStatus", (req, res) => {
   res.status(200).json({status: "success", version: "1.0"});
@@ -12,12 +14,21 @@ app.get("/backendStatus", (req, res) => {
 // Rotta dedicata al Login
 app.post("/login", (req, res) => {
   // 1° step: recuperare dati ricevuti dal front-end
-  const userEmail = req.params.email;
-  const userPassword = req.params.password;
+  const userEmail = req.body.email;
+  const userPassword = req.body.password;
+  console.log(req.requestOptions)
   if(!userEmail || !userPassword) {
-    res.status(400).json({status: "error"});
+    res.status(400).json({status: "error", userEmail: userEmail, userPassword: userPassword});
+  }else {
+    const user = fakeDB.find(user => user.email === userEmail && user.password === userPassword);
+    if (user) {
+      res.status(200).json({status: "success", user: user});
+    } else {
+      res.status(400).json({status: "email o password errati"});
+    }
   }
   // 2° step: confrontare i dati ricevuti 
+  
   // 3° step: risposta ottenuta di successo o meno
 });
 
